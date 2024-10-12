47f192f65edaf585e7351c963ce785199454417a
StoneyDSP <nathanjhood@googlemail.com>
2024-10-12 21:54:33 +0200

    improved tailwind config
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,10 @@
+/* eslint-disable no-undef */
+import defaults from "./tailwind.defaults";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,html,css}', './public/*.html'],
+  presets: [defaults],
   darkMode: 'media', // or 'class'
   theme: {
     extend: {},
